245ca58af4374241e5a41b658db61e9967bb568b
Kardes Recep <recepkardes89@gmail.com>
2024-03-01 02:09:52 +0300

    v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		container: {
+			center: true,
+			screens:{
+				lg: "1124px",
+				xl: "1124px",
+				"2xl": "1124px",
+			}
+		},
+		extend: {
+			fontFamily: {
+				gemunu: ["Gemunu Libre", "sans-serif"],
+				"open-sans": ["Open Sans", "sans-serif"],
+			},
+			colors: {
+				"movie-red": "#BC1A45",
+				"movie-melon": "#FFD369",
+				"movie-grey": "#DDDDDD",
+				"movie-white": "#F7F7F7",
+			},
+		},
+	},
+	plugins: [],
+};
